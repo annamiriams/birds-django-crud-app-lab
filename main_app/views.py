2f8586186bc5ef4cffa7549ae217912eaf1a4bfc
annams <annaextrasteckel@gmail.com>
2025-05-26 16:45:56 -0700

    CBVs and full CRUD functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Bird
 
 # Create your views here.
@@ -16,3 +17,17 @@ def bird_index(request):
 def bird_detail(request, bird_id):
     bird = Bird.objects.get(id=bird_id)
     return render(request, 'birds/detail.html', {'bird': bird})
+
+class BirdCreate(CreateView):
+    model = Bird
+    fields = '__all__'
+    
+class BirdUpdate(UpdateView):
+    model = Bird
+    # Only allowing updates to description:
+    fields = ['description']
+    
+class BirdDelete(DeleteView):
+    model = Bird
+    # Override the get_absolute_url method now included in the model. Deleting a bird needs to reroute to the /birds/ page since that bird's detail page will no longer exist.
+    success_url = '/birds/'
